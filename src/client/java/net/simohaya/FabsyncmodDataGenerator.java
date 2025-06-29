2a04cf278e99c4f9bfbf518b7c30697348fcd81e
Simohayhe <166674587+Simohayhe@users.noreply.github.com>
2025-06-29 21:11:35 +0900

    t1

---
+++ src/client/java/net/simohaya/FabsyncmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.simohaya;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FabsyncmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
